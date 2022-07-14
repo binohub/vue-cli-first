da04285f0f2b64c9c5538c47f703f7f63c5c6054
binohub <yeebino001@gmail.com>
2022-07-14 17:30:53 +0200

    vueCli - TitleMain - update

--- vue-intro/src/components/TitleMain.vue
+++ vue-intro/src/components/TitleMain.vue
@@ -1,32 +1,16 @@
 <template>
-  <div class="hello">
-
+  <div>
+    <H1>La mia prima app con Vue CLI! 🤤 </H1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TitleMain',
-  props: {
-    msg: String
-  }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
